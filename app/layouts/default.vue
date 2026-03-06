56a34f91338af0cd505229c8bedc07f69ae385b3
joachimhviid <joac1099@gmail.com>
2026-03-06 14:31:30 +0100

    tutorial

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="min-h-screen bg-amber-400">
+  <div class="min-h-screen bg-black text-white md:bg-blue-500">
     <slot />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  css: ['./app/assets/css/main.css'],
+  css: ['./assets/css/main.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/eslint'],
   vite: {
