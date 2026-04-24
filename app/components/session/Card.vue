93f9a8fe3025c8208ff32487d2e707ce5197c5af
joachimhviid <joac1099@gmail.com>
2026-04-24 09:39:33 +0200

    wip: session card component

---
+++ app/components/session/Card.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <!-- Session summary -->
+    <!-- Who participated -->
+    <!-- When did session take place -->
+    <!-- Controls for deleting and/or editing? -->
+  </div>
+</template>
