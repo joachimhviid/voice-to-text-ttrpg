1623e89ea9d691bdb121f680e6f63b5bc5c193e2
joachimhviid <joac1099@gmail.com>
2026-05-01 13:13:50 +0200

    feat: session card component

--- app/components/session/Card.vue
+++ app/components/session/Card.vue
@@ -1,10 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { InternalApi } from 'nitropack'
+
+type SessionData = NonNullable<InternalApi['/api/campaigns/:campaignId']['default']>['sessions'][number]
+defineProps<{
+  session: SessionData
+}>()
+</script>
 
 <template>
-  <div>
+  <div class="flex justify-between rounded border border-white/20 p-4">
     <!-- Session summary -->
+    <span>{{ session.id }}</span>
+    <span>{{ session.status }}</span>
     <!-- Who participated -->
     <!-- When did session take place -->
     <!-- Controls for deleting and/or editing? -->
+    <UiButton variant="secondary">
+      <Icon name="heroicons:chevron-down" size="20px" />
+    </UiButton>
   </div>
 </template>
